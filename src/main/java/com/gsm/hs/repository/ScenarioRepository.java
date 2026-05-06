5a900c716862214b115501dcee99542fd17b0a19
junsu <s26022@gsm.hs.kr>
2026-05-06 17:56:57 +0900

    5.18 ai develop에 커밋

--- src/main/java/com/gsm/hs/repository/ScenarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gsm.hs.repository;
-
-import com.gsm.hs.entity.Scenario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
-}
